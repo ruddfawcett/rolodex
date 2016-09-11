36fb1c1921daaf34965fb9d28f89e332f47dc986
Rudd Fawcett <rudd.fawcett@gmail.com>
2016-09-11 01:34:26 -0400

    Added members view

---
+++ public/js/form.js
@@ -0,0 +1,93 @@
+var socket = io('http://localhost:8080');
+var app = feathers().configure(feathers.socketio(socket));
+var events = app.service('/api/events');
+var members = app.service('/api/members');
+
+$('input[name="checkin"]').on('click', function() {
+  var affiliation = $('input[name="affiliation"]').val();
+  var role = $('input[name="role"]').val();
+  var borough = $('select[name="borough"]').val();
+  var teacher = $('select[name="teacher"]').val() === 'true' ? true : false;
+
+  if (affiliation.length == 0 || role.length == 0 || borough.length == 0) {
+    $('.message').text('Please complete your profile!').fadeIn().delay(2000).fadeOut();
+    return;
+  }
+
+  var Teacher = {
+    $set: {
+      complete_profile: true,
+      profile: {
+        affiliation: affiliation,
+        role: role,
+        borough: borough
+      }
+    }
+  };
+
+  if (teacher) {
+    var subjects = {};
+    var raw_subjects = $('select[name="subjects"]').val();
+
+    if ($.inArray('ENGLISH', raw_subjects) > -1) {
+      subjects.english = true;
+    }
+    if ($.inArray('MATHEMATICS', raw_subjects) > -1) {
+      subjects.mathematics = true;
+    }
+    if ($.inArray('SCIENCE', raw_subjects) > -1) {
+      subjects.science = true;
+    }
+    if ($.inArray('SOCIALSTUDIES', raw_subjects) > -1) {
+      subjects.social_studies = true;
+    }
+    if ($.inArray('FOREIGNLANGUAGE', raw_subjects) > -1) {
+      subjects.foriegn_language = true;
+    }
+    if ($.inArray('ART', raw_subjects) > -1) {
+      subjects.art = true;
+    }
+    if ($.inArray('COMPUTERSCIENCE', raw_subjects) > -1) {
+      subjects.computer_science = true;
+    }
+
+    var grades = [];
+    var raw_grades = $('select[name="grades"]').val();
+    if ($.inArray('ELEMENTARY', raw_grades) > -1) {
+      grades.elementary = true;
+    }
+    if ($.inArray('MIDDLE', raw_grades) > -1) {
+      grades.middle_school = true;
+    }
+    if ($.inArray('HIGH', raw_grades) > -1) {
+      grades.high_school = true;
+    }
+    if ($.inArray('POSTHIGH', raw_grades) > -1) {
+      grades.post_high = true;
+    }
+
+    Teacher.$set.profile = {
+      teacher: true,
+      subjects_taught: subjects,
+      grade_levels: grades
+    }
+
+    if (subjects.length == 0 || grades.length == 0) {
+      $('.message').text('Please choose subjects or grades!').fadeIn().delay(2000).fadeOut();
+      return;
+    }
+  }
+
+  members.update(member_id, Teacher).then((result) => {
+    if (result) {
+      console.log('asdf');
+      window.location.reload();
+    }
+    else {
+      console.log('error');
+    }
+  }).catch((error) => {
+    console.log('error');
+    console.log(error);
+  });
+});
--- src/routes/members.js
+++ src/routes/members.js
@@ -2,9 +2,39 @@ const app = require('../app');
 const router = require('feathers').Router();
 
 const members = app.service('/api/members');
+const events = app.service('/api/events');
 
-router.get('/:member_id/:member_name', (req, res) => {
-
+router.get('/:event_id/:member_id', (req, res) => {
+  members.get(req.params.member_id).then((member) => {
+    if (!member) {
+      return next(new Error('Member not found.'));
+    }
+    else {
+      if (member.complete_profile) {
+        events.update(req.params.event_id, {
+          $addToSet: { 'attendees.yes': req.params.member_id }
+        }).then((result) => {
+          if (!result) { return next(new Error('Couldn\'t update.')); }
+          else {
+            return res.render('checked-in', {
+              event: result
+            });
+          }
+        }).catch((error) => {
+          return next(error);
+        });
+      }
+      else {
+        return res.render('member', {
+          member: member,
+          event_id: req.params.event_id,
+          member_id: req.params.member_id
+        });
+      }
+    }
+  }).catch((error) => {
+    return next(error);
+  })
 });
 
 module.exports = router;
